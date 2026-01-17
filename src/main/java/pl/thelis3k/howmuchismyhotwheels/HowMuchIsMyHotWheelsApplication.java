298746c624188a97dbf9188938c6989f8ccc53da
Ksawery <ksawery2k19@gmail.com>
2026-01-17 22:05:33 +0100

    chore: initial project structure setup

---
+++ src/main/java/pl/thelis3k/howmuchismyhotwheels/HowMuchIsMyHotWheelsApplication.java
@@ -0,0 +1,13 @@
+package pl.thelis3k.howmuchismyhotwheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowMuchIsMyHotWheelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowMuchIsMyHotWheelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/thelis3k/howmuchismyhotwheels/HowMuchIsMyHotWheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.thelis3k.howmuchismyhotwheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowMuchIsMyHotWheelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
